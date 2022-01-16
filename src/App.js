c97df9400ede5379ad1f549bcd2b3dafffb06315
chocoscoding <oyetit@gmail.com>
2022-01-16 12:31:06 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import './styles/App.css';
 import {Routes, Route, Link} from 'react-router-dom'
 import { db } from './firebase-config';
@@ -17,18 +17,18 @@ import { collection, getDocs } from 'firebase/firestore';
 function App() {
 
   const [projects, setProjects] = useState([]);
-  const userCollection = collection(db, 'projects');
-
+  
+  const getProjects = useCallback( async () => {
+    const userCollection = collection(db, 'projects');
+    const data = await getDocs(userCollection);
+    const response = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    setProjects(response)
+    console.log(response);
+  }, [])
   useEffect(() => {
-    const getProjects = async () => {
-      const data = await getDocs(userCollection);
-      const response = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
-      setProjects(response)
-      console.log(response);
-    }
     getProjects();
 
-  }, [])
+  },[getProjects])
   return (
     <div className="App">
       fklkflsdfl
